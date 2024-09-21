0a8aa35913c7398b1c7f29958e4b01045d07c5bd
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-09-21 18:07:51 +0530

    sefesf

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json());
 app.use(cookieParser());
 const corsOption={
-    origin:'http://localhost:5173',
+    origin:'https://human-bot-connect-app.vercel.app',
     credentials:true,
 }
 app.use(cors(corsOption))
